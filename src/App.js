72c9ec0530c11c6f2769a654c7717879d872c3ee
RobertAron <Robert.aron.tx@verizon.net>
2018-04-21 21:46:12 -0500

    router added

--- src/App.js
+++ src/App.js
@@ -3,37 +3,26 @@ import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import InputForm from "./components/input-form"
+import MapDetails from "./components/MapDetails"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import SimpleMap from './components/SimpleMap'
 
 
 class App extends Component {
-  state = {
-    origin: '',
-    destination: 'Arlingon, tX'
-  }
-
   render() {
-    const { origin, destination } = this.state;
-
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <InputForm />
-          <SimpleMap
-            ref={map => { this.map = map; }}
-          />
-          <input
-            placeholder="start address here"
-            onChange={e => this.setState({ origin: e.target.value })}
-          />
-          <button onClick={() => this.map.drawRoute(origin, destination, 'FLIGHT')}>Draw route!</button>
-        </div>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <Route exact path="/" component={InputForm} />
+            <Route path="/Map" component={MapDetails} />
+          </div>
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }
---
+++ src/components/MapDetails.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import SimpleMap from './SimpleMap'
+
+class MapDetails extends Component {
+
+    state = {
+        origin: '',
+        destination: 'Arlingon, tX'
+    }
+
+    render() {
+        const { origin, destination } = this.state;
+        return (
+            <div>
+                <SimpleMap
+                    ref={map => { this.map = map; }}
+                />
+                <input
+                    placeholder="start address here"
+                    onChange={e => this.setState({ origin: e.target.value })}
+                />
+                <button onClick={() => this.map.drawRoute(origin, destination, 'FLIGHT')}>Draw route!</button>
+            </div >
+        )
+    }
+
+}
+
+export default MapDetails;
