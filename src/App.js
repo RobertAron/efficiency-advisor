42459be434aebadfcf2632856c1fbc5438c35451
maryehuerta <mary.huerta@mavs.uta.edu>
2018-04-21 20:23:19 -0500

    added Form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import InputForm from "./components/input-form"
+
 
 
 class App extends Component {
@@ -13,9 +15,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <InputForm />
         </div>
       </MuiThemeProvider>
     );
