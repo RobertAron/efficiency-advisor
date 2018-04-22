8cad165c6dfcbeacd09d6a5fbc922de3c5062066
Zachery Gentry <zacherygentry@gmail.com>
2018-04-22 03:44:13 -0500

    Woosh

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,7 @@ import Divider from 'material-ui/Divider';
 import './Homepage.css';
 import StateFarm from '../images/State-farm.png'
 import Southwest from '../images/southwest.png'
+import Uber from '../images/uber.png'
 import { RaisedButton } from 'material-ui';
 import { Link } from 'react-router-dom'
 import './input-form.js'
@@ -13,7 +14,6 @@ export default class App extends Component {
     super(props);
 
     this.goForward = this.goForward.bind(this)
-    this.goUber = this.goUber.bind(this)
 
     this.state = {
 
@@ -33,52 +33,69 @@ export default class App extends Component {
           <p className="Text-center">A Fuel Saver and Money Saver</p>
           <h2 className="Text-center"> What is the most energy efficient mode of transportation for my trip? </h2>
           <RaisedButton
-            backgroundColor = "#27ae60"
-            labelStyle = {{
+            backgroundColor="#27ae60"
+            labelStyle={{
               textTransform: 'none',
               color: 'white',
             }}
-            className = "Button"
-            buttonStyle={{ 
-              height: '50px' 
+            className="Button"
+            buttonStyle={{
+              height: '50px'
             }}
             label="Create a trip"
             onClick={this.goForward}>
-            
+
           </RaisedButton>
         </div>
 
         <div className="Footer">
           <Card className="Slide"
-          zDepth = {3}
-          onClick = {() => window.location = 'https://www.statefarm.com/insurance/auto/discounts/drive-safe-save'}
+            zDepth={3}
+            onClick={() => window.location = 'https://www.statefarm.com/insurance/auto/discounts/drive-safe-save'}
           >
             <CardMedia
               className="Image">
               <img src={StateFarm} alt="" />
             </CardMedia>
-            <Divider/>
+            <Divider />
             <CardTitle
               style={{
                 margin: '15px'
               }}
               className="Title"
-              title="Get insurance Rewards with State Farm" subtitle="State Farm® is a long-time proponent of safe driving. But did you know that safer driving habits can also bring greener driving results?" />
+              title="Get insurance rewards with State Farm" subtitle="State Farm® is a long-time proponent of safe driving. But did you know that safer driving habits can also bring greener driving results?" />
             <CardText>
               Learn more about State Farm's Drive Safe and Save
             </CardText>
-            <CardActions>
-            </CardActions>
           </Card>
           <Card className="Slide"
-          zDepth = {3}
-          onClick = {() => window.location = 'https://www.uber.com/ride/'}>
-
+            zDepth={3}
+            onClick={() => window.location = 'https://www.uber.com/en-BO/ride/uberpool/'}>
+            <CardMedia
+              style={{
+                width: '200px',
+                marginTop: '50px',
+                display: 'inline-block',
+                paddingBottom: '33px'
+              }}
+              className="Image">
+              <img src={Uber} alt="" />
+            </CardMedia>
+            <Divider />
+            <CardTitle
+              style={{
+                margin: '15px'
+              }}
+              className="Title"
+              title="Try carpooling with Uber" subtitle="" />
+            <CardText>
+              With uberPOOL, everybody wins. Including the environment.
+            </CardText>
           </Card>
 
           <Card className="Slide"
-          zDepth = {3}
-          onClick = {() => window.location = 'https://www.southwest.com/flight/shortcut/low-fare-search.html'}>
+            zDepth={3}
+            onClick={() => window.location = 'https://www.southwest.com/flight/shortcut/low-fare-search.html'}>
             <CardMedia
               style={{
                 width: '225px',
